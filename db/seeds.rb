7a68854c3e6b94a5527049fd9602910ab74b215c
phuongthanh96 <phuongthanhpham1996@gmail.com>
2018-05-18 21:21:52 +0700

    finish devise log in

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,3 +41,12 @@ Film.all.each do |film|
     FilmOrigin.create(film_id: film.id, origin_id:genre.id)
   end
 end
+
+10.times do |n|
+  User.create(
+    email: "user#{n+1}@gmail.com",
+    password: "123456",
+    confirmed_at: Time.now,
+    confirmation_token: nil)
+end
+
