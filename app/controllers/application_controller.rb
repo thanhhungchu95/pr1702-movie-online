025f0e1b2d8b356e9605eeb44547826c29735a0d
quachhuytung <huytung.9729@gmail.com>
2018-05-31 23:53:53 +0700

    drop-down-menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  before_action :load_search
+  before_action :load_search, :load_origins, :load_genres
 
   private
   def load_search
   	@q = Film.ransack(params[:q])
+  end
+
+  def load_origins
+    @origins = Origin.all
+  end
+
+  def load_genres
+    @genres = Genre.all
   end
 end
